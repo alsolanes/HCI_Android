31fd187c5bc89d5b3ce405ca26791e7154c40e75
Aleix Solanes <al.solanes@gmail.com>
2016-06-11 12:03:41 +0200

    Image compression before upload & location available
    Still not uploading location data, but ready to begin with that

--- KeepMyTown/app/src/main/java/project/hci/keepmytown/MainActivity.java
+++ KeepMyTown/app/src/main/java/project/hci/keepmytown/MainActivity.java
@@ -1,5 +1,13 @@
 package project.hci.keepmytown;
 
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.ProgressDialog;
@@ -12,6 +20,7 @@ import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
@@ -21,11 +30,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
+import android.text.format.DateFormat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.jar.Manifest;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, LocationListener{
     public static final String UPLOAD_URL = "http://keepmytown.esy.es/PhotoUpload/upload.php";
     public static final String UPLOAD_KEY = "image";
     private int PICK_IMAGE_REQUEST = 1;
@@ -39,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Bitmap bitmap;
 
     private Uri filePath;
+
+    private String provider;
+    private double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +72,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonChoose.setOnClickListener(this);
         buttonUpload.setOnClickListener(this);
         buttonView.setOnClickListener(this);
+
+        getCoords();
     }
 
     private void showFileChooser() {
@@ -81,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public String getStringImage(Bitmap bmp){
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        bmp.compress(Bitmap.CompressFormat.JPEG, 50, baos);
         byte[] imageBytes = baos.toByteArray();
         String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
         return encodedImage;
@@ -95,6 +115,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             protected void onPreExecute() {
+                /** SHOW LOCATION BEFORE UPLOAD
+                Context context = getApplicationContext();
+                CharSequence text = "Loc:"+String.valueOf(latitude)+ " "+String.valueOf(longitude);
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                 **/
                 super.onPreExecute();
                 loading = ProgressDialog.show(MainActivity.this, "Uploading...", null,true,true);
             }
@@ -124,6 +151,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ui.execute(bitmap);
     }
 
+
+
     @Override
     public void onClick(View v) {
         if (v == buttonChoose) {
@@ -141,5 +170,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void viewImage() {
         startActivity(new Intent(this, ImageListView.class));
+    }
+
+    public void getCoords() {
+        // Set up location manager for getting GPS coordinates
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // If GPS is not enabled, take user to screen to enable it
+        boolean enabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        // Get last known location
+        boolean permissionGranted = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        if(permissionGranted) {
+            // {Some Code}
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            onLocationChanged(location);
+        } else {
+            latitude = -1.0;
+            longitude = -1.0;
+        }
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
